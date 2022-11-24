85b873a7e3c94056356d4d91c6698ebe7453775b
Federico Zona Le Fort <fzonalefort@gmail.com>
2022-11-23 21:51:37 -0300

    Capas

--- Tercera entrega/services/info.js
+++ Tercera entrega/services/info.js
@@ -3,12 +3,12 @@ const data = {
   Nombre_de_Plataforma: process.platform,
   Version_Node: process.version,
   Path_de_Ejecucion: process.execPath,
-  Process_id: process.pid,  
-  Carpet_de_Proyecto: process.cwd(),  
-}
+  Process_id: process.pid,
+  Carpet_de_Proyecto: process.cwd(),
+};
 
 const getInfo = () => {
   return data;
-}
+};
 
 export { getInfo };
