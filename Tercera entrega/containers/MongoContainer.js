059e92491f76ddf1008f2104e428da3a5577961f
Federico Zona Le Fort <fzonalefort@gmail.com>
2022-11-28 14:13:40 -0300

    API arquitectura

--- Tercera entrega/containers/MongoContainer.js
+++ Tercera entrega/containers/MongoContainer.js
@@ -1,63 +1,49 @@
 /* import mongo conection */
-import '../connection/connection.js'; //corroborar si asi se conecta
+import "../connection/connection.js"; //corroborar si asi se conecta
 
 class MongoContainer {
-
   constructor(schema) {
-    this.schema = schema;    
+    this.schema = schema;
   }
 
   async list() {
     try {
-      const arr = await this.schema.find({});  
+      const arr = await this.schema.find({});
       return arr;
     } catch (error) {
       console.log(error);
     }
   }
 
-  async getById(idEl) {    
+  async getById(idEl) {
     try {
-      const el = await this.schema.findOne({ _id: idEl }) //en mongo el id se guarda en _id      
+      const el = await this.schema.findOne({ _id: idEl }); //en mongo el id se guarda en _id
       return el;
     } catch (err) {
-      return({error: 'elemento no encontrado'});
-    }
-  }
-
-  async save(obj) {
-    try { 
-      let timestamp = new Date().getTime(); //VERIFICAR FUNCION **** 
-      obj.timestamp = timestamp   //VERIFICAR FUNCION ****  
-      const newObj = new this.schema(obj);
-      const data = await newObj.save();  
-      return data;
-    } catch (error) {
-      console.log(error);
+      return { error: "elemento no encontrado" };
     }
   }
 
   async deleteById(idEl) {
-    try {      
-      const data = await this.schema.findByIdAndDelete(idEl)       
-      return ('elemento Eliminado')
+    try {
+      const data = await this.schema.findByIdAndDelete(idEl);
+      return "elemento Eliminado";
     } catch (error) {
       console.log(error);
     }
   }
 
-  async changeById(idEl, obj) {    
+  async changeById(idEl, obj) {
     try {
-      let timestamp = new Date().getTime(); //VERIFICAR FUNCION **** 
-      obj.timestamp = timestamp   //VERIFICAR FUNCION **** 
-      
-      const el = await this.schema.findByIdAndUpdate(idEl, obj)      
-      return ('elemento Actualizado') 
+      let timestamp = new Date().getTime(); //VERIFICAR FUNCION ****
+      obj.timestamp = timestamp; //VERIFICAR FUNCION ****
+
+      const el = await this.schema.findByIdAndUpdate(idEl, obj);
+      return "elemento Actualizado";
     } catch (error) {
       console.log(error);
     }
-  }  
-  
+  }
 }
 
 export default MongoContainer;
--- Tercera entrega/public/main.js
+++ Tercera entrega/public/main.js
@@ -3,36 +3,34 @@ const socket = io.connect();
 
 //funcion para renderizar los productos
 const renderProducts = () => {
-  const productsContainer = document.getElementById("productsContainer");     
+  const productsContainer = document.getElementById("productsContainer");
   productsContainer.innerHTML = "";
 
-  console.log('probando inicio');
-  fetch('/lista-productos')
-  .then(response => response.json())
-  .then(data => { 
-      console.log('probando data', data);
-      
-      data.forEach(el => {
-      productsContainer.innerHTML += `
-      <div class="productContainer">
-              <div class="productImg">
-                  <img src=${el.thumbnail} alt="imagen de ${el.name}">
-              </div>
-              <div class="productInfo">
-                  <div>
-                      <h3>${el.name}</h3>
-                      <p>$${el.price}</p>
-                  </div>
-                  <p class="info">${el.description}</p>         
-              </div>
+  console.log("probando inicio");
+  fetch("/lista-productos")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("probando data", data);
+
+      data.forEach((el) => {
+        productsContainer.innerHTML += `
+        <div class="productContainer">
+                <div class="productImg">
+                    <img src=${el.thumbnail} alt="imagen de ${el.name}">
+                </div>
+                <div class="productInfo">
+                    <div>
+                        <h3>${el.name}</h3>
+                        <p>$${el.price}</p>
+                    </div>
+                    <p class="info">${el.description}</p>         
+                </div>
           </div>
       `;
       });
-  })
-  .catch(err => console.log(err));  
-}
-
-
+    })
+    .catch((err) => console.log(err));
+};
 
 //funcion que se encarga de renderizar el listado de productos
 /* function renderProducts() {
@@ -54,28 +52,31 @@ const renderProducts = () => {
 
 //funcion que se encarga de renderizar los mensajes
 function renderMessages(data) {
-  const html = data.map((el, index) => {
-      return(`<div>
+  const html = data
+    .map((el, index) => {
+      return `<div>
           <p class="emailMensaje">${el.email}</p>
           <p class="fechaMensaje">[${el.fecha}]:</p>
-          <p class="mensajeMensaje">${el.mensaje}</p> </div>`)
-  }).join(" ");
-  document.getElementById('mensajes').innerHTML = html;
+          <p class="mensajeMensaje">${el.mensaje}</p> </div>`;
+    })
+    .join(" ");
+  document.getElementById("mensajes").innerHTML = html;
 }
 
 //funcion que toma la info de los mensajes
 function addMessage(e) {
   const mensaje = {
-      email: document.getElementById('mail').value,
-      fecha: `${(new Date).toLocaleDateString()} - ${(new Date).toLocaleTimeString()}`,      
-      mensaje: document.getElementById('mensaje').value
-  };    
-  socket.emit('new-message', mensaje);
+    email: document.getElementById("mail").value,
+    fecha: `${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}`,
+    mensaje: document.getElementById("mensaje").value,
+  };
+  socket.emit("new-message", mensaje);
   return false;
 }
 
-socket.on('productos', function() { renderProducts(); });
-socket.on('messages', function(data) { renderMessages(data); });
-
-
-
+socket.on("productos", function () {
+  renderProducts();
+});
+socket.on("messages", function (data) {
+  renderMessages(data);
+});
